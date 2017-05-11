64c71195b26cc719926836048d5fa576845674cc
bs3589 <blakesmith3589@gmail.com>
2017-05-11 12:45:46 -0400

    added first and last name to user table, changed business_show page to display user name on their posts

--- squarefare/app/controllers/application_controller.rb
+++ squarefare/app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # before_action :authenticate_user!
+
+  before_action :add_parameters_to_user, if: :devise_controller?
+
+  def add_parameters_to_user
+  	# devise_parameter_sanitizer.for(:sign_up) << :first_name
+  	# devise_parameter_sanitizer.for(:sign_up) << :last_name
+  	# devise_parameter_sanitizer.for(:account_update) << :first_name
+  	# devise_parameter_sanitizer.for(:account_update) << :last_name
+
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name]) 
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+  	devise_parameter_sanitizer.permit(:account_update, keys: [:first_name])
+  	devise_parameter_sanitizer.permit(:account_update, keys: [:last_name])	
+  end
 end
